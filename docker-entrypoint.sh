590883282ae6f348a35e2616fb5225518bf3df3b
Clément Blanco <clement.blanco@gmail.com>
2018-03-21 15:35:49 +0000

    Removing permissions fix ran at every startup
    We'll add a way to do it only once during build + at runtime with an command/alias

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,10 +6,4 @@ if [ "false" == "$XDEBUG" ]; then
     sed -i "s/^/;/" /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
 fi
 
-# Magento 2 permissions
-find /var/www/var /var/www/vendor /var/www/generated /var/www/pub/static /var/www/pub/media /var/www/app/etc -type f -exec chmod g+w {} \;
-find /var/www/var /var/www/vendor /var/www/generated /var/www/pub/static /var/www/pub/media /var/www/app/etc -type d -exec chmod g+ws {} \;
-chown -R www-data:www-data /var/www
-chmod u+x /var/www/bin/magento
-
 exec "$@"
